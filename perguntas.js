d654c21c5f79c6dbabee596817325fd10d15fbe1
Giovana <111807601+giovanawx98@users.noreply.github.com>
2024-08-30 21:47:11 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual é o genêro de filmes que você mais gosta? (escolha entre drama e terror)",
+        alternativas: [
+            {
+                texto: "A) Drama",
+                afirmacao: [
+                    "Você gosta de histórias emocionantes e profundas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Terror",
+                afirmacao: [
+                    "Pra sentir aquele frio na espinha."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Já assistiu filmes estrangeiros? (fora de Hollywood)",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Gosto de explorar diferentes culturas.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Prefiro filmes de Hollywood."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "3. O último filme que você assistiu foi um clássico ou um lançamento recente?",
+        alternativas: [
+            {
+                texto: "Um filme recente.",
+                afirmacao: [
+                    "Direto dos cinemas!!"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Um clássico.",
+                afirmacao: [
+                    "Um filme antigo, mas inesquecível!"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "4. Onde você prefere assistir filmes?",
+        alternativas: [
+            {
+                texto: "Cinema.",
+                afirmacao: [
+                    "Gosto da tela grande e o som potente."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Em casa.",
+                afirmacao: [
+                    "Gosto do conforto do sofá e pipoca caseira."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "5. Tem algum filme que você assistiu mais de 3 vezes?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Não canso de rever meus filmes favoritos!"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Prefiro ver coisas novas?"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "6. Você gosta de filmes sobre heróis?",
+        alternativas: [
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Gosto da aventura."
+                ],
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "São muito genéricos, não há surpresas."
+                ],
+            },
+        ]
+    },
+];
